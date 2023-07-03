82d7323fd1d2f4fcd96997f98a44d86af3264e4c
whrind <59040632+whrind@users.noreply.github.com>
2023-07-03 23:44:07 +0800

    Add files via upload

--- minigame.py
+++ minigame.py
@@ -1,29 +1,26 @@
-line0 = ["O", "O", "1", "O", "O"]
-line1 = ["O", "O", "O", "O", "O"]
-line2 = ["O", "O", "O", "O", "O"]
-line3 = ["O", "O", "O", "O", "O"]
-line4 = ["O", "O", "O", "0", "O"]
-line5 = ["O", "O", "O", "O", "O"]
-line6 = ["O", "O", "O", "O", "O"]
-line7 = ["O", "O", "O", "O", "O"]
+mapp = [["O", "O", "1", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"],
+        ["O", "O", "O", "0", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"]]
+lines = len(mapp)
 
 
 def main():
-    print(f"{line0}\n{line1}\n{line2}\n{line3}\n"
-          f"{line4}\n{line5}\n{line6}\n{line7}\n")
+    for i in range(lines):
+        print(mapp[i])
 
 
 def getpo():
-    r = [chec(line0), chec(line1), chec(line2), chec(line3),
-         chec(line4), chec(line5), chec(line6), chec(line7)]
-    print(r)
+    r = []
+    for i in range(lines):
+        try:
+            r.append(mapp[i].index("1"))
+        except:
+            r.append(0)
+    return r
 
 
-def chec(list):
-    try:
-        return list.index("1")
-    except:
-        return 0
-
-
-getpo()
+def delself(lis):
+    global po, i
+    for i in range(lines):  # which lines
+        if lis[i] != 0:
+            po = lis[i]
+    mapp.remove(i[po])
