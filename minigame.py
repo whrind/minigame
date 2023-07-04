ead9efd1bf28f64979bc77915cbccb342c80c887
whrind <59040632+whrind@users.noreply.github.com>
2023-07-05 00:00:19 +0800

    Add files via upload

--- minigame.py
+++ minigame.py
@@ -1,26 +1,47 @@
-mapp = [["O", "O", "1", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"],
-        ["O", "O", "O", "0", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"]]
+mapp = [["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"],
+        ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "1"]]
 lines = len(mapp)
 
 
 def main():
+    showmapp()
+    movew(getpo())
+    showmapp()
+    pass
+
+
+def showmapp():
+    print("----------------------------")
     for i in range(lines):
         print(mapp[i])
 
 
-def getpo():
-    r = []
-    for i in range(lines):
-        try:
+def getpo():                                             # 0 --------> x +
+    global x, y                                          # |
+    r = []                                               # |
+    for i in range(lines):                               # |
+        try:                                             # y +
             r.append(mapp[i].index("1"))
         except:
             r.append(0)
-    return r
-
-
-def delself(lis):
-    global po, i
     for i in range(lines):  # which lines
-        if lis[i] != 0:
-            po = lis[i]
-    mapp.remove(i[po])
+        if r[i] != 0:
+            x = r[i]     # the "1" horizon position
+            y = i          # the "1" vertical position
+    return [x, y]
+
+
+def dele(coord):   # lis = r
+    mapp[coord[1]].pop(coord[0])
+    mapp[coord[1]].insert(coord[0], "O")
+
+
+def movew(coord):   # (x, y-1)
+    newcoord = [coord[0], coord[1]-1]
+    mapp[newcoord[1]].pop(newcoord[0])
+    mapp[newcoord[1]].insert(newcoord[0], "1")
+    dele(coord)
+
+
+main()
+
